f401cbe30ad971eb370d5bd3677832f04d3d9d2c
sho <shocho0101@hotmail.co.jp>
2017-10-11 17:56:26 +0900

    基本的な機能の完成

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //SKViewに型を変換する
+        let skView = self.view as! SKView
+        
+        //FPSの表示
+        skView.showsFPS = true
+        
+        //ノード数の表示
+        skView.showsNodeCount = true
+        
+        //viewと同じサイズでsceneを設定
+        let scene = GameScene(size: skView.frame.size)
+        
+        //ビューにシーンを表示
+        skView.presentScene(scene)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override var prefersStatusBarHidden: Bool {
+        get{
+            return true
+        }
+    }
 
 }
 
